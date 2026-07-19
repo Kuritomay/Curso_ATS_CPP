94a32f35b44fed8ac80e806bd9c7ec53284194e6
Elias <eliascur16@gmail.com>
2026-07-19 18:11:52 -0500

    Voy con los datos basicos

---
+++ 01_Fundamentos/Entrada_y_Salida/002-tipos-de-datos.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+  //Numero natural
+  int natural = 23; //Se podria usar para edad, pequena cantidades. 
+
+  //Numero racional
+  float racional-peque = 3.14; //medida de una persona lo podrias poner un f para decir que es grande
+  double racional = 3.1415; //Mas preciso, se podria usar para numero racioanles como para pi, talla de una persona. 
+  
+  //Caracter
+  char carater = 'j'; //Solo usar un bit, pero puede definri si quieres mas grande.
+  
+  //Boleano 
+  bool booleano = true; //True o False, para decir verdad y falso. 
+
+  return 0;
+}
